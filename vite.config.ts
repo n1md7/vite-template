26d42136d096d4dfe89aa38b469ac2bfbb800eee
nimda <b.kodua@gmail.com>
2024-03-02 19:38:58 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@
 import { defineConfig } from 'vite';
 import { envSchema } from './src/common/validations/env.schema';
 
-import glsl from 'vite-plugin-glsl';
 import solidPlugin from 'vite-plugin-solid';
+import glsl from 'vite-plugin-glsl';
+import devtools from 'solid-devtools/vite';
 
 export default defineConfig(({ mode }) => {
   envSchema.validateSync({ mode }, { strict: true });
@@ -37,7 +38,7 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-    plugins: [solidPlugin(), glsl()],
+    plugins: [glsl(), devtools(), solidPlugin()],
     test: {
       setupFiles: ['./tests/unit/__setup__/setup.ts'],
       globals: true,
